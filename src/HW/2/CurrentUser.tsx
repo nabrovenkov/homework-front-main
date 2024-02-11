02975198a7e20c22dc66fc2b470a54901b9a23c2
Alexey Nabrovenkov <nabrovenkov@gmail.com>
2024-02-11 20:46:48 +0300

    1

--- src/HW/2/CurrentUser.tsx
+++ src/HW/2/CurrentUser.tsx
@@ -1,10 +1,11 @@
 import { UserType } from './HW2';
 
 type CurrentUserPropsType = {
-  user: any // пропиши типизацию
+  user: UserType // пропиши типизацию
 };
 
 export const CurrentUser = (props: CurrentUserPropsType) => {
+  console.log(props.user.id);
   return (
     <li key={props.user.id} id={`hw02-user-${props.user.id}`}>
       <strong>{props.user.name}</strong> (Age: {props.user.age})<strong> Address: </strong>
